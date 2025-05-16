5563e02dcddf151f6381a4c2f7f1591b6275921a
cf <cf.gg.tty@hotmail.com>
2025-05-16 09:21:37 -0400

    Initial commit

---
+++ Makefile
@@ -0,0 +1,31 @@
+PREFIX  := /usr/local
+CC      := gcc
+CFLAGS  := -pedantic -Wall -Wno-deprecated-declarations -Os
+LDFLAGS := -lX11
+
+all: options dwmblocks
+
+options:
+	@echo dwmblocks build options:
+	@echo "CFLAGS  = ${CFLAGS}"
+	@echo "LDFLAGS = ${LDFLAGS}"
+	@echo "CC      = ${CC}"
+
+dwmblocks: dwmblocks.c blocks.def.h blocks.h
+	${CC} -o dwmblocks dwmblocks.c ${CFLAGS} ${LDFLAGS}
+
+blocks.h:
+	cp blocks.def.h $@
+
+clean:
+	rm -f *.o *.gch dwmblocks
+
+install: dwmblocks
+	mkdir -p ${DESTDIR}${PREFIX}/bin
+	cp -f dwmblocks ${DESTDIR}${PREFIX}/bin
+	chmod 755 ${DESTDIR}${PREFIX}/bin/dwmblocks
+
+uninstall:
+	rm -f ${DESTDIR}${PREFIX}/bin/dwmblocks
+
+.PHONY: all options clean install uninstall
---
+++ patches/blocks.h
@@ -0,0 +1,48 @@
+/*           __       _                    _     _            _
+ *      ___ / _|   __| |_      ___ __ ___ | |__ | | ___   ___| | _____
+ *     / __| |_   / _` \ \ /\ / / '_ ` _ \| '_ \| |/ _ \ / __| |/ / __|
+ *    | (__|  _| | (_| |\ V  V /| | | | | | |_) | | (_) | (__|   <\__ \
+ *     \___|_|    \__,_| \_/\_/ |_| |_| |_|_.__/|_|\___/ \___|_|\_\___/
+ */
+
+typedef struct {
+  const char *icon;
+  const char *command;
+  unsigned int interval;
+  unsigned int signal;
+} Block;
+
+static const Block blocks[] = {
+    /*Icon*/        /*Command*/                            /*Update Interval*/    /*Update Signal*/
+    {"",       "cat /tmp/recordingicon 2>/dev/null",                0,                      9},
+    /* {"",       "cat /tmp/mpvqicon      2>/dev/null",                0,                     15}, */
+    {"",       "cat /tmp/walfehicon    2>/dev/null",                0,                     16},
+    {"",       "sb-mpvq",                                           0,                     15},
+    {"",       "sb-tasks",                                         10,                     26},
+    {"",       "sb-music",                                          0,                     11},
+    {"",       "sb-pacpackages",                                    0,                      8},
+    {"",       "sb-torrent",                                       20,                      7},
+    {"",       "sb-memory",                                        10,                     14},
+    {"",       "sb-disk",                                           0,                     13},
+    {"",       "sb-cpu",                                           10,                     18},
+    {"",       "sb-news",                                           0,                      6},
+    {"",       "sb-moonphase",                                   8000,                     17},
+    {"",       "sb-forecast",                                    3600,                      5},
+    /* {"",       "sb-mailbox",                                     1800,                     12}, */
+    {"",       "sb-volume",                                         0,                     10},
+    {"",       "sb-battery",                                        5,                      3},
+    {"",       "sb-internet",                                       5,                      4},
+    {"",      "sb-kbselect",                                       0,                     30},
+    {"",       "sb-clock ",                                         1,                      1},
+    /* {"",       "sb-price xmr Monero XMR 24",                     9000,                     24}, */
+    /* {"",       "sb-price eth Ethereum ETH 23",                   9000,                     23}, */
+    /* {"",       "sb-price btc Bitcoin BTC 21",                    9000,                     21}, */
+    /* {"",       "sb-doppler",                                        0,                     13}, */
+    /* {"",       "sb-nettraf",                                        1,                     16}, */
+    /* {"",       "sb-ipaddress",                                      0,                     27}, */
+    /* {"",       "sb-help-icon",                                      0,                     15}, */
+};
+
+/* Block delimiter. NULL character `\0' means no delimiter. */
+static const char *delim = " \0";
+
